1d9a0df2e37affa0e1d8cb0807f2b3bf2e585917
dannyarabejo <dannyarabejo@yahoo.com>
2025-09-17 15:51:31 -0700

    Add files via upload

---
+++ Excersise One.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excersise_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the name of gas station: ");
+            string gasStation = Console.ReadLine();
+
+            Console.Write("Enter the price of gas: ");
+            double gasprice = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("How many gallons: ");
+            double gallons = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("What is the tax percentage for this transaction: ");
+            double taxpercent = Convert.ToDouble(Console.ReadLine());
+
+            // calculate 
+            double subtotal = gasprice * gallons;
+
+            // calculate tax
+            double tax = subtotal * (taxpercent / 100);
+
+            // calculate total
+            double total = subtotal + tax;
+
+            // display 
+            Console.WriteLine();
+            Console.WriteLine("******* " + gasStation + " *******");
+            Console.WriteLine("Gas price: $" + gasprice.ToString("0.00"));
+            Console.WriteLine("Number of gallons: " + gallons);
+            Console.WriteLine("Tax rate: " + taxpercent + "%");
+            Console.WriteLine("Subtotal: $" + subtotal.ToString("0.00"));
+            Console.WriteLine("Tax: $" + tax.ToString("0.00"));
+            Console.WriteLine("Total: $" + total.ToString("0.00"));
+
+           
+        }
+    }
+}
